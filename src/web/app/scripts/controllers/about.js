244bc5833f4a4dfcb9592b5c27bb4ad77c498420
StephaneMangin <stephane.mangin@freesbee.fr>
2015-11-06 09:06:17 +0100

    Create Interface branch with angularJs.

---
+++ src/web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name agileManagerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the agileManagerApp
+ */
+angular.module('agileManagerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ src/web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name agileManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the agileManagerApp
+ */
+angular.module('agileManagerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
